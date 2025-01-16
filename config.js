1c0f7af86e4e7bd540f958ce8ffe1baa06c54fbd
Kakashi hatake <105074780+kakashuuu@users.noreply.github.com>
2025-01-17 01:55:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const session = process.env.SESSION || '';
-const mongoURI = process.env.MONGO_URI || '';
+const mongoURI = process.env.MONGO_URI || 'mongodb+srv://kaizer:kaizer@cluster0.v0nls.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const mycode = process.env.CODE || '254';
 const botname = process.env.BOTNAME || 'DREADED';
 const herokuAppName = process.env.HEROKU_APP_NAME || '';
